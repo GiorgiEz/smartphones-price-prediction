b8249484671d0f72b1e9028064daba7100e3118b
giorgi <liparteliani.giorgi@kiu.edu.ge>
2025-10-21 00:55:55 +0400

    removed unnecessary texts in readme

--- src/main/Main.py
+++ src/main/Main.py
@@ -1,6 +1,7 @@
 from src.data_processing.RunDataProcessing import RunDataProcessing
 from src.machine_learning.RunML import RunML
 from src.price_prediction.predict import predict
+from src.exploratory_data_analysis.RunEDA import RunEDA
 
 
 if __name__ == '__main__':
@@ -9,8 +10,8 @@ if __name__ == '__main__':
     data_processing.run_process()  # Perform data processing on the datasets
 
     """ Part 2: Exploratory Data Analysis """
-    # exploratory_data_analysis = RunEDA()  # Call exploratory data analysis class
-    # exploratory_data_analysis.run_visualizations()  # Run the visualizations of the dataset
+    exploratory_data_analysis = RunEDA()  # Call exploratory data analysis class
+    exploratory_data_analysis.run_visualizations()  # Run the visualizations of the dataset
 
     """ Part 3: Machine Learning """
     machine_learning = RunML()  # Call Machine Learning class
